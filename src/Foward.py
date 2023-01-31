cc82ddac558ec457c9b4233e4740f355b32f53e1
reidm28 <mark.reid28@mail.dcu.ie>
2023-01-31 14:02:12 -0800

    Dataset A1D Organised into columns/rows

---
+++ src/Foward.py
@@ -0,0 +1,14 @@
+
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Define the columns of the dataframe
+columns = ['Datetime', 'HC Air temperature [°C] mean', 'HC Air temperature [°C] max', 'HC Air temperature [°C] min',
+           'Dew Point [°C] mean', 'Dew Point [°C] min', 'HC Relative humidity [%] mean', 'HC Relative humidity [%] max',
+           'HC Relative humidity [%] min', 'Precipitation [mm] sum', 'Leaf Wetness [min] time', 'Battery voltage [mV] last']
+
+# Create the dataframe
+df = pd.read_csv('A1D.csv',encoding = 'unicode_escape', names=columns, skiprows=1, parse_dates=[0])
+
+
+print(df)
