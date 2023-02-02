4b4d95720e7569cd28a7017e9d68b0a220e6d15b
reidm28 <mark.reid28@mail.dcu.ie>
2023-02-02 13:27:40 -0800

    Expanded on TimeSeriesMissing scenario by allowing the user to key in specific year, month or day they want to set as null in the dataframe.

--- src/Gap-Insertion-Methods/TimeSeriesMissing.py
+++ src/Gap-Insertion-Methods/TimeSeriesMissing.py
@@ -2,17 +2,8 @@ import pandas as pd
 import numpy as np
 
 
-def insert_gaps_into_dataset(file_name):
+def change_datetime_in_dataset(file_name, year=None, month=None, day=None):
 
-    """
-
-    Parameters:
-    file_name (str): The name of the CSV file containing the weather dataset.
-
-
-    Returns:
-    pandas.DataFrame: The weather dataset with gaps inserted.
-    """
 
     # Check if the file exists and is a valid CSV file
     if not file_name.endswith('.csv'):
@@ -24,15 +15,29 @@ def insert_gaps_into_dataset(file_name):
         raise FileNotFoundError("File not found")
 
     # Read the CSV file into a pandas DataFrame
-    df = pd.read_csv("../Hourly Datasets (CSV)/" + file_name, sep=',', skiprows = 1, encoding='latin1')
-    df['Datetime'] = np.nan #Time-Series Data Removed
+    df = pd.read_csv("../Hourly Datasets (CSV)/" + file_name, sep=',', skiprows=1, encoding='latin1')
+
+    # Convert the Datetime column to datetime type
+    df['Datetime'] = pd.to_datetime(df['Datetime'])
+    # Null conversion for day, month & year
+    if year:
+        df.loc[df['Datetime'].dt.year == year, 'Datetime'] = np.nan
+    if month:
+        df.loc[df['Datetime'].dt.month == month, 'Datetime'] = np.nan
+    if day:
+        df.loc[df['Datetime'].dt.day == day, 'Datetime'] = np.nan
+
     print(df.head())
 
-
-
-    # return df
+    return df
 
 
 if __name__ == '__main__':
     file_name = input("Enter the name of the csv file: ")
-    df = insert_gaps_into_dataset(file_name)
+    year = input("Enter the year (optional): ")
+    year = int(year) if year.strip() else None
+    month = input("Enter the month (optional): ")
+    month = int(month) if month.strip() else None
+    day = input("Enter the day (optional): ")
+    day = int(day) if day.strip() else None
+    df = change_datetime_in_dataset(file_name, year, month, day)
