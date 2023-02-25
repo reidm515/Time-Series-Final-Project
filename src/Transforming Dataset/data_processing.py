107d58dd59ffc8d22cf20eed3490bbe66374b178
boylek42 <kevin.boyle42@mail.dcu.ie>
2023-02-25 12:40:46 +0000

    Created the three functions for processing the dataset. They are; Clean Dataset - Removes Missing and Invalid Values, Z-Score Outlier Removal and Interquartile Range Outlier Removal.

--- src/Transforming Dataset/data_processing.py
+++ src/Transforming Dataset/data_processing.py
@@ -2,12 +2,61 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
+data = pd.read_csv("../Hourly Datasets (CSV)/")
 cleaned_dataset = []
 
-# Function to remove outliers using Z-Scores
-def remove_outliers(data):
-    threshold = 3
+def clean_dataset(data):
+
+    # Remove missing and invalid values from Dataset.
+
+    # Parameters:
+    # data: Dataset/ CSV file containing the weather dataset after being converted to Pandas Dataframe.
+
+    # Returns:
+    # Dataset with outliers removed using Z-Scores
+    
+    data.dropna(inplace = True)
+
+def outlier_removal_zScore(data, threshold):
+    
+    # Function to remove outliers from hourly datasets using Z-Scores
+
+    # Parameters:
+    # data: Dataset/ CSV file containing the weather dataset after being converted to Pandas Dataframe.
+    # threshold: The maximum Z-score value before a data point is considered an outlier
+
+    # Returns:
+    # Dataset with outliers removed using Z-Scores
+
+    if threshold <= 0:
+        raise ValueError("The threshold value must be a postive number.")
+
     mean = np.mean(data)
     std = np.std(data)
-    cleaned_dataset = data[np.abs(data - mean) / std) <= threshold]
+    cleaned_dataset = data[(np.abs(data - mean) / std) <= threshold]
+    return cleaned_dataset
+
+
+def outlier_removal_IQR(data, column_name):
+
+    # Function to remove outliers from hourly datasets using Interquartile Range 
+
+    # Parameters:
+    # data: Dataset / CSV file containing the weather dataset after being converted to Pandas Dataframe.
+    #column: Column in dataset that outliers will be removed from.
+
+    # Returns:
+    # Dataset with outliers removed using Interquartile Range
+    
+    if column_name not in data.columns:
+        raise ValueError("The column name is not present in Dataset.")
+
+    Q1 = data.height.quantile(0.25)
+    Q3 = data.height.quantile(0.75)
+    IQR  = Q3 - Q1
+
+    lower_limit = Q1 - (IQR * 1.5)
+    upper_limit = Q1 + (IQR * 1.5)
+
+    cleaned_dataset = data[(data.height < lower_limit) & (data.height < upper_limit)]
     return cleaned_dataset
