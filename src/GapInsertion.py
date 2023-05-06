58dea83c947c93bbcc618188a0512d67062a5ff6
Mark Reid <mark.reid28@mail.dcu.ie>
2023-05-06 22:00:50 +0000

    Update GapInsertion.py

--- src/GapInsertion.py
+++ src/GapInsertion.py
@@ -1,121 +1,75 @@
 import pandas as pd
 import numpy as np
-
-def insert_gaps(file_name, method, num_rows=None, num_gaps=None, gap_size=None):
-
-    if method == 'random-sampling':
-        return randomsampling(file_name, num_rows)
-    elif method == 'TimeSeriesMissing':
-        return TimeSeriesMissing(file_name)
-    elif method == 'spatially-temporally-correlated':
-        return spatially_temporally_correlated_gaps(file_name, num_gaps, gap_size)
-    else:
-        raise ValueError("Invalid method. Please choose from 'random-sampling', 'TimeSeriesMissing', or 'spatially-temporally-correlated'.")
-
-def randomsampling(file_name, num_rows):
-    # The function reads the dataset from a CSV file, generates random row indices, and sets the values for the selected rows to NaN.
+import random
 
 
+def random_gaps(file, gap_count, gap_size):
+    # Generate gap_count random numbers in the range of 0 and len(dataset)
+    gap_indices = random.sample(range(len(file)), gap_count)
 
-    # This Checks if the file is a valid CSV file and actually exists
-    if not file_name.endswith('.csv'):
-        raise ValueError("The input file needs to be in CSV format.")
-    try:
-        with open("Hourly Datasets (CSV)/" + file_name, 'r') as f:
-            pass
-    except FileNotFoundError:
-        raise FileNotFoundError("File not found")
+    for idx in gap_indices:
+        file[idx:idx + gap_size + 1] = np.nan
 
-    # Read the CSV file into a pandas DataFrame
-    df = pd.read_csv("Hourly Datasets (CSV)/" + file_name, sep=',', skiprows=1, encoding='latin1')
-    print(df.head())
+    # Print the list of random indices
+    #print(f"Inserted {gap_count} of size {gap_size} gaps at: ")
+    #for i in gap_indices:
+        #print(i)
 
-    # Generate the random row indices
-    random_row_indices = np.random.permutation(num_rows)
+    return file
 
-    # Select the rows based on the random indices and set their values to NaN
-    return df.iloc[random_row_indices, :]
+def annual_maintenance_gaps(df, maintenance_duration):
 
-    # return df
+    # Make a copy of the input DataFrame
+    df_copy = df.reindex(index=df.index[::-1])
 
-def spatially_temporally_correlated_gaps(file_name, num_gaps, gap_size):
-    # This function inserts spatially and temporally correlated gaps into our dataset by selecting a group of contiguous rows and setting their values to NaN.
 
-    # Check if the file exists and is a valid CSV file
-    if not file_name.endswith('.csv'):
-        raise ValueError("File must be a CSV file")
-    try:
-        with open("Hourly Datasets (CSV)/" + file_name, 'r') as f:
-            pass
-    except FileNotFoundError:
-        raise FileNotFoundError("File not found")
 
-    # The function uses pandas read_csv function to read the CSV file into a pandas DataFrame.
-    # It skips the first row and sets the delimiter to a comma.
-    df = pd.read_csv("Hourly Datasets (CSV)/" + file_name, sep=',', skiprows=1, encoding='latin1')
 
-    # The function converts the 'Datetime' column to a datetime type using pandas to_datetime function
-    df['Datetime'] = pd.to_datetime(df['Datetime'])
+    # Get the start and end dates for the DataFrame
+    start_of_year = df.resample('D').first().iloc[0].name
+    end_of_year = df.resample('D').last().iloc[-1].name
 
-    # The function generates a list of random starting indices for the gaps using numpy random choice function.
-    # It uses the number of gaps and gap size arguments to determine the number of rows in each gap.
-    starting_indices = np.random.choice(range(len(df) - gap_size), num_gaps, replace=False)
+    # Number of observations per year
+    observations_per_year = df.loc[(df.index >= start_of_year) & (df.index <= end_of_year)].shape[0]
 
-    # Set the values for each group of contiguous rows to NaN
-    # The function sets the values of the selected rows for each gap to NaN using the pandas iloc function.
-    for start_idx in starting_indices:
-        end_idx = start_idx + gap_size - 1
-        df.iloc[start_idx:end_idx + 1, 1:] = np.nan
+    # Choose random date for maintenance
+    maintenance_interval = random.randint(1, observations_per_year)
 
+    # Set the value at the maintenance interval to NaN
+    df.iloc[maintenance_interval, :] = np.nan
+
+    # Set values at yearly intervals to NaN
+    for i in range(maintenance_interval + observations_per_year, len(df), observations_per_year):
+        df.iloc[i:(i + maintenance_duration), :] = np.nan
     return df
 
-def TimeSeriesMissing(file_name):
-    if not file_name.endswith('.csv'):
-        raise ValueError("File must be a CSV file")
-    try:
-        with open("Hourly Datasets (CSV)/" + file_name, 'r') as f:
-            pass
-    except FileNotFoundError:
-        raise FileNotFoundError("File not found")
-    df = pd.read_csv("Hourly Datasets (CSV)/" + file_name, sep=',', skiprows=1, encoding='latin1')
+def weather_outage_gaps(file, num_outage_days, outage_duration, col):
+    df_copy = file.copy()
 
+    # Get the first and last years in the dataset
+    first_year = pd.to_datetime(df_copy.index.min()).year
+    last_year = pd.to_datetime(df_copy.index.max()).year
 
-    df['Datetime'] = pd.to_datetime(df['Datetime'])
-    while True:
-        year = input("Enter the year (optional), or 'done' if finished: ")
-        if year.lower() == 'done':
-            break
-        year = int(year) if year.strip() else None
+    # iterate through each year and insert gaps.
+    while (int(first_year) != (int(last_year))):
+        gap_period_start = pd.Timestamp(year=first_year, month=7, day=1, hour=0)
+        gap_period_end = pd.Timestamp(year=first_year, month=9, day=1, hour=23)
 
-        month = input("Enter the month (optional): ")
-        month = int(month) if month.strip() else None
+        # Random dates between gap_period_start to gap_period_end
+        gap_dates = pd.date_range(start=gap_period_start, end=gap_period_end, freq='H')
+        gap_dates = random.sample(list(gap_dates), k=num_outage_days)
 
-        day = input("Enter the day (optional): ")
-        day = int(day) if day.strip() else None
+        # Add object to each Timestamp object in the list
+        gap_end = [x + pd.Timedelta(hours=outage_duration) for x in gap_dates]
 
-        if year:
-            df.loc[df['Datetime'].dt.year == year, 'Datetime'] = np.nan
-        if month:
-            df.loc[df['Datetime'].dt.month == month, 'Datetime'] = np.nan
-        if day:
-            df.loc[df['Datetime'].dt.day == day, 'Datetime'] = np.nan
+        # Set the values in the selected rows to NaN
+        for gap_start, gap_stop in zip(gap_dates, gap_end):
+            mask = (df_copy.index >= gap_start) & (df_copy.index <= gap_stop)
+            df_copy.loc[mask, col] = np.nan
 
-    return df
-
-if __name__ == '__main__':
-    file_name = input("Enter the name of the csv file: ")
-    choice = input("Enter 'random-sampling' or 'TimeSeriesMissing' or 'spatially-temporally-correlated' to choose the program to run: ")
-
-    if choice == 'random-sampling':
-        num_rows = int(input("Enter the number of rows to sample: "))
-        df = insert_gaps(file_name, choice, num_rows=num_rows)
-    elif choice == 'TimeSeriesMissing':
-        df = insert_gaps(file_name, choice)
-    elif choice == 'spatially-temporally-correlated':
-        num_gaps = int(input("Enter the number of gaps to insert: "))
-        gap_size = int(input("Enter the size of each gap: "))
-        df = insert_gaps(file_name, choice, num_gaps=num_gaps, gap_size=gap_size)
-    else:
-        print("Invalid choice. Please enter 'random-sampling' or 'TimeSeriesMissing' or 'spatially-temporally-correlated'")
-
-    print(df.head())
+        # print(gap_dates)
+        # Increment year counter
+        first_year += 1
+        df_copy = df_copy.reindex(index=df_copy.index[::-1])
+    # Return the modified copy of the DataFrame
+    return df_copy
